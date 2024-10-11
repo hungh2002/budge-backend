ccde3ad85ed57455a9204561842b37830427dc3c
hungh2002 <hungg562002@gmail.com>
2024-10-11 12:51:13 +0900

    init

---
+++ src/main/java/com/hungh2002/budget_backend/BudgetBackendApplication.java
@@ -0,0 +1,13 @@
+package com.hungh2002.budget_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudgetBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hungh2002/budget_backend/BudgetBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hungh2002.budget_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
